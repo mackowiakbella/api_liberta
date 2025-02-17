d9287b46c26b771f44b65d830593038ec01fd12c
mackowiakbella <isabellamdallabrida@gmail.com>
2025-02-17 13:41:23 -0400

    commit

--- index.js
+++ index.js
@@ -1,10 +1,70 @@
+import cors from 'cors';
 import express from 'express';
 //import pool from './servico/conexao.js';
 import { retornaCampeonatos, retornaCampeonatosID, retornaCampeonatosAno, retornaCampeonatosTime} from './servico/retornaCampeonatos_servico.js';
 import { cadastraCampeonato } from './servico/cadastroCampeonato_servico.js';
+import { atualizaCampeonato, atualizaCampeonatoParcial } from './servico/atualizaCampeonato_servico.js';
+import { deletaCampeonato } from './servico/deletaCampeonato_servico.js';
 
 const app = express();
+app.use(cors());
 app.use(express.json()); //Suporte para JSON no corpo(body) da requisição
+
+app.delete('/campeonatos/:id', async (req, res) => {
+    const {id} = req.params;
+    
+    if (isNaN(id)) {
+        res.status(404).send("Parâmetro inválido");
+    }
+    else {
+        const resultado = await deletaCampeonato(id);
+    
+        if (resultado.affectedRows > 0) {
+            res.status(202).send("Registro deletado com sucesso");
+        } else {
+            res.status(404).send("Registro não encontrado");
+        }
+    }
+})
+
+app.path('/campeonatos/id:', async (req, res) => {
+    const {id} = req.params;
+    const {campeao, vice, ano} = req.body;
+
+    const camposAtualizar = {}; //{campeao: "flamengo"}
+    if(campeao) camposAtualizar.campeao = campeao;
+    if(vice) camposAtualizar.vice = vice;
+    if(ano) camposAtualizar.ano = ano;
+
+    if(Object.keys(camposAtualizar).length === 0) {
+        res.status(400).send("Nenhum campo válido foi enviado para atualização");
+    } else {
+        const resultado = await atualizaCampeonatoParcial(id, camposAtualizar);
+
+        if (resultado.affectedRows > 0) {
+            res.status(202).send("O registro foi atualizado com sucesso");
+        } else {
+            res.status(404).send("Registro não encontrado");
+        }
+    }
+})
+
+app.put('/campeonatos/:id', async(req, res) => {
+    const {id} = req.params;    //desestruturação
+    const {campeao, vice, ano} = req.body;
+
+    if (campeao == undefined || vice == undefined || ano == undefined) {
+        res.status(400).send('Todos os campos devem ser informados')
+    } else {
+        const resultado = await atualizaCampeonato(id, campeao, vice, ano)
+        if (resultado.affectedRows > 0) {
+            res.status(202).send('Registro atualizado com sucesso');
+        } else {
+            res.status(404).send('Registro não encontrado');
+        }
+        
+    }
+}) 
 
 app.post('/campeonatos', async (req, res) => {
     const campeao = req.body.campeao;
